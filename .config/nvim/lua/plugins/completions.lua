009a07223cbb9546e0ff9bf1d82be672d23d33ee
Ripply <husseinharakeh96@gmail.com>
2024-07-26 15:38:21 +0300

    Update confirm auto completion to Tab

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -34,7 +34,7 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({
+                    ['<Tab>'] = cmp.mapping.confirm({
                         select = true
                     }),
                 }),

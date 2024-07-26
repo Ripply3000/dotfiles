6a7312530f982e09fafe429f8d4004a9cd75cbc6
Ripply <husseinharakeh96@gmail.com>
2024-07-26 15:33:33 +0300

    Nvim telescope updates

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,44 +1,49 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-                defaults = {
-                    layout_strategy = "vertical",
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-                            ["<C-j>"] = require("telescope.actions").move_selection_next,
-                        }
-                    },
-                }
-            })
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				defaults = {
+					layout_strategy = "vertical",
+					mappings = {
+						i = {
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+						},
+					},
+				},
+				-- pickers = {
+				-- 	buffers = {
+				-- 		previewer = false,
+				-- 	},
+				-- },
+			})
 
-            -- TODO: Check if this can be done in the default mappings
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			-- TODO: Check if this can be done in the default mappings
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-            require("telescope").load_extension("ui-select")
-            require("telescope").load_extension("fzf")
-        end,
-    },
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }

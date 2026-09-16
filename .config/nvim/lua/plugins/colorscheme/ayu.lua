7ad4139378076650325c6dca3a7c946bf978811b
Ripply <husseinharakeh96@gmail.com>
2026-09-16 16:51:55 +0300

    nvim(add): ayu theme

--- .config/nvim/lua/plugins/colorscheme/ayu.lua
+++ .config/nvim/lua/plugins/colorscheme/ayu.lua
@@ -5,7 +5,18 @@ vim.pack.add({
 require('ayu').setup({
     mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
     terminal = true, -- Set to `false` to let terminal manage its own colors.
-    overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+    -- overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+    overrides = {
+        Normal = { bg = "None" },
+        NormalFloat = { bg = "none" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        VertSplit = { bg = "None" },
+    },
 })
 
 vim.cmd("colorscheme ayu")
